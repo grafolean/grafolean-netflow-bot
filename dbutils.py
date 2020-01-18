1e49fda08ad4009048fbd2f8e9f562e9c0f12666
Anze <anze@grafolean.com>
2020-01-18 15:04:23 +0100

    Create index on netflow_records (ts)

--- dbutils.py
+++ dbutils.py
@@ -137,3 +137,7 @@ def migration_step_2():
     with db.cursor() as c:
         c.execute(f'CREATE TABLE {DB_PREFIX}records (seq BIGSERIAL NOT NULL PRIMARY KEY, ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, client_ip TEXT, version SMALLSERIAL NOT NULL);')
         c.execute(f'CREATE TABLE {DB_PREFIX}flows (record INTEGER NOT NULL REFERENCES {DB_PREFIX}records(seq) ON DELETE CASCADE, data JSONB NOT NULL);')
+
+def migration_step_3():
+    with db.cursor() as c:
+        c.execute(f'CREATE INDEX {DB_PREFIX}records_ts ON {DB_PREFIX}records (ts);')
