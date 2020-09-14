3781e42d8390826b4c61b53360c4f79d99234932
Anze <anze@grafolean.com>
2020-09-14 07:34:13 +0200

    Fix: unknown protocol should not throw a KeyError exception

--- netflowbot.py
+++ netflowbot.py
@@ -478,7 +478,8 @@ class NetFlowBot(Collector):
 
             output_path_entity = NetFlowBot.construct_output_path_prefix(interval_label, direction, entity_id, interface=None)
             for protocol, traffic_bytes in c.fetchall():
-                output_path = f"{output_path_entity}.topprotocol.{path_part_encode(PROTOCOLS[protocol])}"
+                protocol_label = PROTOCOLS[protocol] if protocol in PROTOCOLS else f"proto{protocol}"
+                output_path = f"{output_path_entity}.topprotocol.{path_part_encode(protocol_label)}"
                 values.append({
                     'p': output_path,
                     'v': traffic_bytes / time_between,  # Bps
